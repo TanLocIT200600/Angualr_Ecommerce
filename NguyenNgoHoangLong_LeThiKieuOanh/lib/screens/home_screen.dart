397b7060c3ada5b38ccbd189c98fecc93a9f5260
unknown <tanlocit20062000@gmail.com>
2022-05-19 17:19:47 +0700

    update

--- NguyenNgoHoangLong_LeThiKieuOanh/lib/screens/home_screen.dart
+++ NguyenNgoHoangLong_LeThiKieuOanh/lib/screens/home_screen.dart
@@ -47,27 +47,6 @@ class _HomePageState extends State<HomePage> {
                     route: '/add',
                     args: FormArguments(),
                   ),
-                  // Container(
-                  //   margin: EdgeInsets.all(20),
-                  //   width: 200,
-                  //   height: 50,
-                  //   decoration: BoxDecoration(
-                  //     gradient: LinearGradient(
-                  //       colors: [Colors.lightGreen, Colors.orange],
-                  //       begin: FractionalOffset.topLeft,
-                  //       end: FractionalOffset.bottomRight,
-                  //     ),
-                  //   ),
-                  //   child: FlatButton(
-                  //     child: Text(
-                  //       'Login',
-                  //       style: TextStyle(color: Colors.white),
-                  //     ),
-                  //     onPressed: () {
-                  //       Navigator.pushNamed(context, '/add');
-                  //     },
-                  //   ),
-                  // ),
                   const SizedBox(
                     height: 20,
                   ),
@@ -94,67 +73,6 @@ class _HomePageState extends State<HomePage> {
                       route: '$ListScreen',
                     ),
                   ),
-                  // RaisedButton(
-                  //   onPressed: () {
-                  //     Navigator.of(context).push(MaterialPageRoute(
-                  //         builder: (context) => FormStudentScreen()));
-                  //   },
-                  //   shape: RoundedRectangleBorder(
-                  //       borderRadius: BorderRadius.circular(80.0)),
-                  //   padding: const EdgeInsets.all(0.0),
-                  //   child: Ink(
-                  //     child: Container(
-                  //       alignment: Alignment.center,
-                  //       padding: const EdgeInsets.all(10.0),
-                  //       decoration: BoxDecoration(
-                  //           borderRadius: BorderRadius.circular(30),
-                  //           color: Colors.white,
-                  //           gradient: new LinearGradient(
-                  //             colors: [
-                  //               Color.fromARGB(255, 255, 126, 223),
-                  //               Color.fromARGB(255, 255, 182, 80),
-                  //             ],
-                  //           )),
-                  //       child: const Text(
-                  //         'Thêm sinh viên',
-                  //         textAlign: TextAlign.center,
-                  //         style: TextStyle(color: Colors.white, fontSize: 24),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                  // Spacer(
-                  //   flex: 5,
-                  // ),
-                  // RaisedButton(
-                  //   onPressed: () {
-                  //     Navigator.of(context).push(MaterialPageRoute(
-                  //         builder: (context) => ListScreen()));
-                  //   },
-                  //   shape: RoundedRectangleBorder(
-                  //       borderRadius: BorderRadius.circular(80.0)),
-                  //   padding: const EdgeInsets.all(0.0),
-                  //   child: Ink(
-                  //     child: Container(
-                  //       alignment: Alignment.center,
-                  //       padding: const EdgeInsets.all(10.0),
-                  //       decoration: BoxDecoration(
-                  //           borderRadius: BorderRadius.circular(30),
-                  //           color: Colors.white,
-                  //           gradient: new LinearGradient(
-                  //             colors: [
-                  //               Color.fromARGB(255, 255, 126, 223),
-                  //               Color.fromARGB(255, 255, 182, 80)
-                  //             ],
-                  //           )),
-                  //       child: const Text(
-                  //         'Xem danh sách ',
-                  //         textAlign: TextAlign.center,
-                  //         style: TextStyle(color: Colors.white, fontSize: 24),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
                 ],
               ),
             ),
